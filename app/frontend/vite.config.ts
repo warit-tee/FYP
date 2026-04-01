d28e588dfcbb7f1d0d6bbb50f904d86e6bc8db17
Warit Charaspreedalarp <warittee.cha@gmail.com>
2026-04-01 18:26:09 +0800

    change config

--- app/frontend/vite.config.ts
+++ app/frontend/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
-  const proxyTarget = env.VITE_PROXY_TARGET || 'http://localhost:3000'
+  const proxyTarget = env.VITE_PROXY_TARGET || 'http://localhost:8000'
 
   return {
     plugins: [react()],
